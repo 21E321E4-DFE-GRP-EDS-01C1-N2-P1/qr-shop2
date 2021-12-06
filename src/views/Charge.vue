4146b6b4cd49e6c73a0c6b4efa757d86249aec02
Rodrigo <prodrigo@poli.ufrj.br>
2021-12-06 19:57:02 -0300

    fix bug get info charge

--- src/views/Charge.vue
+++ src/views/Charge.vue
@@ -4,62 +4,62 @@
 
     <b-card no-body class="overflow-hidden my-5" v-else>
       <b-row no-gutters>
-      <b-col md="6">
-        <b-img :src="produto.image" alt="Image" fluid center/>
-      </b-col>
-      <b-col md="6" >
-        <b-card-body>
-          <b-card-title>{{ produto.name }}</b-card-title>
-          <b-card-sub-title>R$ {{ produto.price }}</b-card-sub-title>
-          <b-card-text>{{ produto.description }}</b-card-text>
-        </b-card-body>
-        <div class="text-center" v-if="loading" style="margin-top: 80px">
-          <b-spinner variant="success" label="Spinning"></b-spinner>
-        </div>
+        <b-col md="6">
+          <b-img :src="produto.image" alt="Image" fluid center />
+        </b-col>
+        <b-col md="6">
+          <b-card-body>
+            <b-card-title>{{ produto.name }}</b-card-title>
+            <b-card-sub-title>R$ {{ produto.price }}</b-card-sub-title>
+            <b-card-text>{{ produto.description }}</b-card-text>
+          </b-card-body>
+          <div class="text-center" v-if="loading" style="margin-top: 80px">
+            <b-spinner variant="success" label="Spinning"></b-spinner>
+          </div>
 
-        <b-container v-else>
-          <div v-if="charge.produto">
-            <b-img v-bind:src="charge.qrCode" fluid center/>
-          </div>
-          <div v-else>
-            <b-form @submit="handleGetQrCode">
-              <b-form-group>
-                <b-form-input
-                  name="name"
-                  placeholder="Nome"
-                  required
-                  v-model="unknownUser.name"
-                  class='form-input'
-                />
-                <b-form-input
-                  name="cpf"
-                  placeholder="CPF"
-                  required
-                  v-model="unknownUser.cpf"
-                  class='form-input'
-                />
-                <b-form-input
-                  name="endereco"
-                  required
-                  placeholder="Endereco"
-                  v-model="unknownUser.endereco"
-                  class='form-input'
-                />
-                <div style="display: flex">
-                  <b-button
-                    block
-                    variant="outline-secondary"
-                    class="clickable"
-                    type="submit"
-                  >
-                    Pegar QRCode
-                  </b-button>
-                </div>
-              </b-form-group>
-            </b-form>
-          </div>
-        </b-container>
-      </b-col>
+          <b-container v-else>
+            <div v-if="charge.produto">
+              <b-img v-bind:src="charge.qrCode" fluid center />
+            </div>
+            <div v-else>
+              <b-form @submit="handleGetQrCode">
+                <b-form-group>
+                  <b-form-input
+                    name="name"
+                    placeholder="Nome"
+                    required
+                    v-model="unknownUser.name"
+                    class="form-input"
+                  />
+                  <b-form-input
+                    name="cpf"
+                    placeholder="CPF"
+                    required
+                    v-model="unknownUser.cpf"
+                    class="form-input"
+                  />
+                  <b-form-input
+                    name="endereco"
+                    required
+                    placeholder="Endereco"
+                    v-model="unknownUser.endereco"
+                    class="form-input"
+                  />
+                  <div style="display: flex">
+                    <b-button
+                      block
+                      variant="outline-secondary"
+                      class="clickable"
+                      type="submit"
+                    >
+                      Pegar QRCode
+                    </b-button>
+                  </div>
+                </b-form-group>
+              </b-form>
+            </div>
+          </b-container>
+        </b-col>
       </b-row>
     </b-card>
   </div>
@@ -80,6 +80,7 @@ export default {
         this.$store.state.produtos[this.$store.state.selectedProdutoId],
       concluida: false,
       loading: false,
+      interval: null,
     };
   },
   methods: {
@@ -87,10 +88,12 @@ export default {
       console.log("4");
       this.interval = setInterval(async () => {
         try {
-          let info = await chargeService.getChargeInfo(this.charge._id);
-          if (info.status === "CONCLUIDA") {
-            this.concluida = true;
-            clearInterval(this.interval);
+          if (this.interval) {
+            let info = await chargeService.getChargeInfo(this.charge._id);
+            if (info.status === "CONCLUIDA") {
+              this.concluida = true;
+              clearInterval(this.interval);
+            }
           }
         } catch (e) {
           console.log("e", e);
@@ -138,7 +141,7 @@ export default {
     }
   },
 
-  unmounted() {
+  destroyed() {
     clearInterval(this.interval);
   },
   mounted() {
@@ -151,6 +154,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
