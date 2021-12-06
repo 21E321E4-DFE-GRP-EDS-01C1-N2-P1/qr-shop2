4c770e39b8e882ad384dd29ee15c5b03448fb3de
Thyago Teles <thyago.ferreira@al.infnet.edu.br>
2021-12-06 15:10:39 -0300

    melhorias na home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <h2 class="titulo">Home page , Faca login</h2>
+<b-container>
+  <div v-if="isLogged">
+    <h4 class="titulo">Olá! {{this.$store.state.auth && this.$store.state.auth.name}}</h4>
+    <h2 class="titulo">Bem Vindo ao QR-Shop!</h2>
+  </div>
+  <div v-else>
+    <h2 class="titulo">Bem Vindo ao QR-Shop!</h2>
+    <h4 class="titulo">Para mais opções, faça login</h4>
+  </div>
+</b-container>
 </template>
 
 <script>
-// @ is an alias to /src
+import { getTokenLocalStorage } from "../localstorage/tokenStorage";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      isLogged: getTokenLocalStorage()
+    }
+  },
 };
 </script>
 
