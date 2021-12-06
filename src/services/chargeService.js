ec91e432d438774207a0e06e97a43cac6a63131f
Rodrigo <prodrigo@poli.ufrj.br>
2021-12-05 22:13:12 -0300

    Charge when unkown

--- src/services/chargeService.js
+++ src/services/chargeService.js
@@ -4,12 +4,17 @@ function getCharge(produtoId) {
   return axios.get(`/charge/${produtoId}`);
 }
 
+function getChargeUnkown(produtoId, unknownUser) {
+  return axios.post(`/charge/${produtoId}`, unknownUser);
+}
+
 function getChargeInfo(chargeId) {
   return axios.get(`/charge/info/${chargeId}`);
 }
 
 const chargeService = {
   getCharge,
+  getChargeUnkown,
   getChargeInfo,
 };
 
--- src/views/Charge.vue
+++ src/views/Charge.vue
@@ -1,19 +1,55 @@
 <template>
   <div class="container">
     <h1 v-if="concluida">Pagamento concluido!</h1>
-    <div class="text-center" v-else-if="loading">
-      <b-spinner variant="success" label="Spinning"></b-spinner>
-    </div>
+
     <div class="card pagamentos-card" v-else>
       <img v-bind:src="produto.image" height="400px" width="400px" />
       <div class="card-detail">
         <h1>{{ produto.name }}</h1>
         <h2>R$ {{ produto.price }}</h2>
         <h6>{{ produto.description }}</h6>
-        <div v-if="charge.produto">
-          <img v-bind:src="charge.qrCode" height="250px" width="250px" />
+        <div class="text-center" v-if="loading" style="margin-top: 80px">
+          <b-spinner variant="success" label="Spinning"></b-spinner>
         </div>
-        <div v-else />
+        <div v-else>
+          <div v-if="charge.produto">
+            <img v-bind:src="charge.qrCode" height="250px" width="250px" />
+          </div>
+          <div v-else>
+            <b-form @submit="handleGetQrCode">
+              <b-form-group>
+                <b-form-input
+                  name="name"
+                  placeholder="Nome"
+                  required
+                  v-model="unknownUser.name"
+                />
+                <b-form-input
+                  name="cpf"
+                  placeholder="CPF"
+                  required
+                  v-model="unknownUser.cpf"
+                />
+                <b-form-input
+                  name="endereco"
+                  required
+                  placeholder="Endereco"
+                  v-model="unknownUser.endereco"
+                />
+                <div style="display: flex">
+                  <b-button
+                    block
+                    variant="outline-secondary"
+                    style="margin-top: 0; margin-left: 5px"
+                    class="clickable"
+                    type="submit"
+                    >Pegar QRCode</b-button
+                  >
+                </div>
+              </b-form-group>
+            </b-form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -26,6 +62,7 @@ import { getTokenLocalStorage } from "../localstorage/tokenStorage";
 export default {
   data() {
     return {
+      unknownUser: {},
       charge: {},
       produto:
         this.$store.state.produtos &&
@@ -56,22 +93,32 @@ export default {
       const selectedProdutoId = this.$store.state.selectedProdutoId;
 
       try {
-        console.log("1");
-        console.log(selectedProdutoId);
         if (!selectedProdutoId) {
           return this.$router.push("/produtos");
         }
         const charge = await chargeService.getCharge(selectedProdutoId);
-        console.log("charge", charge);
         this.charge = charge;
-        console.log("this.charge", this.charge);
         this.loading = false;
-        console.log("3");
         this.getInfoChargeLoop();
       } catch (e) {
         this.loading = false;
         alert(e.error || e);
-        console.log("e", e);
+      }
+    },
+    async handleGetQrCode(e) {
+      e.preventDefault();
+      this.loading = true;
+      try {
+        const charge = await chargeService.getChargeUnkown(
+          this.$store.state.selectedProdutoId,
+          this.unknownUser
+        );
+        this.charge = charge;
+        this.loading = false;
+        this.getInfoChargeLoop();
+      } catch (e) {
+        this.loading = false;
+        alert(e.error || e);
       }
     },
   },
@@ -80,13 +127,16 @@ export default {
       return this.$router.push("/produtos");
     }
   },
+
+  unmounted() {
+    clearInterval(this.interval);
+  },
   mounted() {
     const isLoggedIn = getTokenLocalStorage();
     if (isLoggedIn) {
-      console.log("1");
       return this.getCharge();
     }
-    return alert("IS NOT LOGGED IN");
+    return;
   },
 };
 </script>
