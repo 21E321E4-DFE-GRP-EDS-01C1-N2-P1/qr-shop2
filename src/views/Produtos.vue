208b25a32adbf55c944ce5c866e713adce578373
Thyago Teles <thyago.ferreira@al.infnet.edu.br>
2021-12-06 14:49:19 -0300

    filtro funcionando

--- src/views/Produtos.vue
+++ src/views/Produtos.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <b-container fluid>
     <b-button
       class="clickable m-3"
       @click="isCreatingProduct = true"
@@ -14,8 +14,23 @@
     <div class="text-center" v-if="loading">
       <b-spinner variant="success" label="Spinning"></b-spinner>
     </div>
+
     <b-container fluid v-else>
-      <b-row style="justify-content: space-evenly;" >
+
+    <b-input-group class="my-5 text-center" style="flex-direction: column;">
+        <label for="filtro"><h5>Busca:</h5></label>
+        <b-form-input placeholder="Buscar um produto" type="search" v-model="filtro" :id='filtro' style="width: 100%"></b-form-input>
+    </b-input-group>
+
+      <b-row style="justify-content: space-evenly;" v-if="filtro">
+        <ProdutoItem
+          :produto="produto"
+          v-for="(produto) in listaFiltrada"
+          :key="produto.id"
+        />
+      </b-row>
+
+      <b-row style="justify-content: space-evenly;" v-else>
         <ProdutoItem
           :produto="produto"
           v-for="produto in Object.values($store.state.produtos)"
@@ -23,7 +38,7 @@
         />
       </b-row>
     </b-container>
-  </div>
+  </b-container>
 </template>
 
 <script>
@@ -40,6 +55,7 @@ export default {
     return {
       isCreatingProduct: false,
       loading: false,
+      filtro: ''
     };
   },
   methods: {
@@ -53,6 +69,27 @@ export default {
         .catch((e) => alert(e))
         .finally(() => (this.loading = false));
     },
+  },
+  computed: {
+    listaFiltrada() {
+      let filterObject = {}
+      let filterArray = Object.values(this.$store.state.produtos).filter((produto) => {
+        const regex = new RegExp(this.filtro, "g")
+        return regex.exec(produto.name) || regex.exec(produto.name.toLowerCase()) || regex.exec(produto.name.toUpperCase()) || regex.exec(produto.price)
+      })
+      filterArray.map(produto => filterObject[produto._id] = produto)
+      return (
+        filterObject
+      )
+    }  
+    // listaFiltrada() {
+    //   return (
+    //     this.produtos.filter((produto) => {
+    //     const regex = new RegExp(this.filtro, "g")
+    //     return regex.exec(produto.titulo) || regex.exec(produto.titulo.toLowerCase()) || regex.exec(produto.titulo.toUpperCase()) || regex.exec(produto.preco)
+    //     })
+    //   )
+    // }  
   },
   mounted() {
     this.carregar();
