d49a928d98f1c24ac338db4cd79945f617613309
Rodrigo <prodrigo@poli.ufrj.br>
2021-12-05 23:44:10 -0300

    Sem produtos

--- src/components/Produtos/DashboardTable.vue
+++ src/components/Produtos/DashboardTable.vue
@@ -2,6 +2,7 @@
   <div class="text-center" v-if="loading">
     <b-spinner variant="success" label="Spinning"></b-spinner>
   </div>
+
   <table class="table" v-else>
     <thead class="thead-dark">
       <tr>
@@ -11,12 +12,20 @@
         <th scope="col"></th>
       </tr>
     </thead>
-    <tbody>
+
+    <tbody v-if="Object.keys($store.state.meusProdutos).length">
       <DashboardItem
         :produto="produto"
         v-for="produto in Object.values($store.state.meusProdutos)"
         :key="produto.id"
       />
+    </tbody>
+    <tbody v-else>
+      <tr>
+        <td colSpan="4" align="center" height="283">
+          <h4 style="margin-top: 80px">Sem Produtos</h4>
+        </td>
+      </tr>
     </tbody>
   </table>
 </template>
@@ -24,6 +33,7 @@
 <script>
 import DashboardItem from "./DashboardItem.vue";
 import produtoService from "../../services/produtoService";
+import { getTokenLocalStorage } from "../../localstorage/tokenStorage";
 
 export default {
   components: {
@@ -37,6 +47,11 @@ export default {
   },
   methods: {
     carregar() {
+      const isLogged = getTokenLocalStorage();
+      console.log("isLogged", isLogged);
+      if (!isLogged) {
+        return this.$router.push("/produtos");
+      }
       this.loading = true;
       return produtoService
         .getMeusProdutos()
