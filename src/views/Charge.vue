7947f4858f4ff0a838e199f193248f104a75ff16
Rodrigo <prodrigo@poli.ufrj.br>
2021-12-05 21:43:20 -0300

    Redesign

--- src/views/Charge.vue
+++ src/views/Charge.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
     <h1 v-if="concluida">Pagamento concluido!</h1>
-    <div class="text-center" v-else-if="loading || !charge.produto">
+    <div class="text-center" v-else-if="loading">
       <b-spinner variant="success" label="Spinning"></b-spinner>
     </div>
     <div class="card pagamentos-card" v-else>
-      <img v-bind:src="charge.qrCode" height="400px" width="400px" />
+      <img v-bind:src="produto.image" height="400px" width="400px" />
       <div class="card-detail">
-        <h1>{{ charge.produto.name }}</h1>
-        <h2>R$ {{ charge.produto.price }}</h2>
-        <h6>{{ charge.produto.description }}</h6>
+        <h1>{{ produto.name }}</h1>
+        <h2>R$ {{ produto.price }}</h2>
+        <h6>{{ produto.description }}</h6>
+        <div v-if="charge.produto">
+          <img v-bind:src="charge.qrCode" height="250px" width="250px" />
+        </div>
+        <div v-else />
       </div>
     </div>
   </div>
@@ -17,17 +21,23 @@
 
 <script>
 import chargeService from "../services/chargeService";
+import { getTokenLocalStorage } from "../localstorage/tokenStorage";
 
 export default {
   data() {
     return {
       charge: {},
+      produto:
+        this.$store.state.produtos &&
+        this.$store.state.selectedProdutoId &&
+        this.$store.state.produtos[this.$store.state.selectedProdutoId],
       concluida: false,
       loading: false,
     };
   },
   methods: {
     getInfoChargeLoop() {
+      console.log("4");
       this.interval = setInterval(async () => {
         try {
           let info = await chargeService.getChargeInfo(this.charge._id);
@@ -46,22 +56,37 @@ export default {
       const selectedProdutoId = this.$store.state.selectedProdutoId;
 
       try {
+        console.log("1");
+        console.log(selectedProdutoId);
         if (!selectedProdutoId) {
-          return this.$router.push("produtos");
+          return this.$router.push("/produtos");
         }
-        this.charge = await chargeService.getCharge(selectedProdutoId);
+        const charge = await chargeService.getCharge(selectedProdutoId);
+        console.log("charge", charge);
+        this.charge = charge;
+        console.log("this.charge", this.charge);
+        this.loading = false;
+        console.log("3");
         this.getInfoChargeLoop();
       } catch (e) {
+        this.loading = false;
         alert(e.error || e);
         console.log("e", e);
-      } finally {
-        this.loading = false;
       }
     },
   },
+  beforeCreate() {
+    if (!this.$store.state.produtos || !this.$store.state.selectedProdutoId) {
+      return this.$router.push("/produtos");
+    }
+  },
   mounted() {
-    console.log("charges");
-    this.getCharge();
+    const isLoggedIn = getTokenLocalStorage();
+    if (isLoggedIn) {
+      console.log("1");
+      return this.getCharge();
+    }
+    return alert("IS NOT LOGGED IN");
   },
 };
 </script>
